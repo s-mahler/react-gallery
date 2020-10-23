9828ab63dc098f658524105242363c6df48cff35
Sam Mahler <mahler.sam@gmail.com>
2020-10-23 16:03:51 -0500

    conditional rendering of pictures and descriptions

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component {
+    
+    state = {
+        showDescription: false
+    }
+
+    renderDescription = () => {
+        console.log('clicked pic', this.state.showDescription);
+        this.setState({
+            showDescription: !this.state.showDescription
+        })
+    }
+    
     render() {
         return (
            <>
-            <img key={this.props.image.id} src={this.props.image.path} alt={this.props.image.description} width="100" height="100"/>
+            {this.state.showDescription === false ?
+                <img onClick={this.renderDescription} key={this.props.image.id} src={this.props.image.path} alt={this.props.image.description} width="100" height="100"/>
+                :
+                <p onClick={this.renderDescription}>{this.props.image.description}</p>
+            }
+            
+            <button>LIKE</button>
            </> 
         )
     }
