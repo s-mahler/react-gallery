7c6cc0f8095d3485d25a3f5ad2c6b6e5f6e0c8c9
Sam Mahler <mahler.sam@gmail.com>
2020-10-22 21:46:21 -0500

    non working GalleryItem prop

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class GalleryItem extends Component {
     render() {
         return (
-            <p>REUTNR v2 Inectpion stiyle</p>
+           <>
+            {/* <img key={this.props.image.id} src={this.props.image.path} alt={this.props.image.description} width="100" height="100"/> */}
+           </> 
         )
     }
 }
