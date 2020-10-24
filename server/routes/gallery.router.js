83652bc1dd160cf6b554e584498144b469d9231b
Sam Mahler <mahler.sam@gmail.com>
2020-10-23 21:09:41 -0500

    convert to DB

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+
+    let galleryId = req.params.id;
+    let sqlText = '';
+
+
+    sqlText = `UPDATE "imageGallery" SET "likes" = "likes" +1 WHERE "id" =$1`;
+
+
+    pool.query(sqlText, [galleryId])
+        .then((result) => {
+            console.log('Successful PUT', result);
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log(`Error in PUT`, error);
+            res.sendStatus(500);
+        })
+
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    const sqlText = `SELECT * FROM "imageGallery" ORDER BY "id";`;
+    pool.query(sqlText)
+        .then((result) => {
+            console.log(`From GET`, result.rows);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error in GET`, error);
+            res.sendStatus(500);
+        })
 }); // END GET Route
 
 module.exports = router;
